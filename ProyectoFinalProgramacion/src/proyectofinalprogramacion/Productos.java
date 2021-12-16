5ad29b93bd3ee6fa00251f75b855ab313c89dc7a
Pablo09fc <pabloferc09@gmail.com>
2021-12-16 13:45:30 -0600

    Proyecto final (completo)
    Código completo del Proyecto Final de Programación

--- ProyectoFinalProgramacion/src/proyectofinalprogramacion/Productos.java
+++
@@ -1,6 +0,0 @@
-package proyectofinalprogramacion;
-
-public class Productos 
-{
-    
-}
--- ProyectoFinalProgramacion/src/proyectofinalprogramacion/ProyectoFinalProgramacion.java
+++ ProyectoFinalProgramacion/src/proyectofinalprogramacion/ProyectoFinalProgramacion.java
@@ -14,10 +14,11 @@ public class ProyectoFinalProgramacion
 {
     public int [][] Precios=new int [3][3];
     private Scanner FIDE= new Scanner (System.in);
+    
    
-        public void Datos_Personales()//Esta clase se utiliza para recuadar los datos del estudiante
+        public void Datos_Personales()//Esta clase se utiliza para recuadar los datos del usuario
         {
-            JOptionPane.showMessageDialog(null, "Se le solicitarán los datos usuario");
+            JOptionPane.showMessageDialog(null, "Se le solicitarán los datos al usuario");
             String cedula= JOptionPane.showInputDialog("Escriba la cédula");
             String nombre=JOptionPane.showInputDialog("Escriba el nombre");
             String apellidos = JOptionPane.showInputDialog("Escriba los apellidos");
@@ -27,7 +28,7 @@ public class ProyectoFinalProgramacion
         
         public void Precios()
         {
-            JOptionPane.showMessageDialog(null, "A continuación se le mostrará la lista de precios de nuestros productos (estos precios cuenta con el servicio de instalación incluido)");
+            JOptionPane.showMessageDialog(null, "A continuación se le mostrará la lista de precios de nuestros productos (estos precios cuenta con el servicio de instalación incluido y sin iva)");
             
             System.out.println("Cabezote Mitsubichi: 100000");
             System.out.println("Cabezote Toyota: 150000");
@@ -68,8 +69,7 @@ public class ProyectoFinalProgramacion
             int Torque3= Precios [2][2];
             
             double iva=0.13;
-            //Combos
-            JOptionPane.showMessageDialog(null, "En este taller vendemos nuestros productos en combo, así que eliga una opción");
+            JOptionPane.showMessageDialog(null, "Eliga una de las siguientes opciones para luego mostrarle su factura");
             System.out.println("1. Cabezote Mitsubichi, Entrada de clutch para Mitsubishi y Torque para llantas Mitsubichi");
             System.out.println("2. Cabezote Toyota, Entrada de clutch para Toyota y Torque para llantas Toyota");
             System.out.println("3. Cabezote Hyundai, Entrada de clutch para Hyundai y Torque para llantas Hyundai");
@@ -82,7 +82,8 @@ public class ProyectoFinalProgramacion
             System.out.println("10. Torque para llantas Mitsubishi");
             System.out.println("11. Torque para llantas Toyota");
             System.out.println("12. Torque para llantas Hyundai");
-            
+            System.out.println();
+            System.out.println();
             
             int opcion=Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el número de la opción requerida "));
        
@@ -91,6 +92,8 @@ public class ProyectoFinalProgramacion
                 int subtotal1= (Cabezote1+Longitud1+Torque1);
                 double ap_iva1= (subtotal1*iva);
                 double total1= (subtotal1+ap_iva1);
+                
+                System.out.println("***Taller Goldoni***");
                 System.out.println("Valor de IVA: "+iva);
                 System.out.println("El subtotal es: "+subtotal1);
                 System.out.println("El total es: "+total1);
@@ -100,6 +103,8 @@ public class ProyectoFinalProgramacion
                 int subtotal2= (Cabezote2+Longitud2+Torque2);
                 double ap_iva2= (subtotal2*iva);
                 double total2= (subtotal2+ap_iva2);
+                
+                System.out.println("***Taller Goldoni***");
                 System.out.println("Valor de IVA: "+iva);
                 System.out.println("El subtotal es: "+subtotal2);
                 System.out.println("El total es: "+total2);
@@ -109,6 +114,8 @@ public class ProyectoFinalProgramacion
                 int subtotal3= (Cabezote3+Longitud3+Torque3);
                 double ap_iva3= (subtotal3*iva);
                 double total3= (subtotal3+ap_iva3);
+                
+                System.out.println("***Taller Goldoni***");
                 System.out.println("Valor de IVA: "+iva);
                 System.out.println("El subtotal es: "+subtotal3);
                 System.out.println("El total es: "+total3);
@@ -118,6 +125,8 @@ public class ProyectoFinalProgramacion
                 int subtotal4= (Cabezote1);
                 double ap_iva4= (subtotal4*iva);
                 double total4= (subtotal4+ap_iva4);
+                
+                System.out.println("***Taller Goldoni***");
                 System.out.println("Valor de IVA: "+iva);
                 System.out.println("El subtotal es: "+subtotal4);
                 System.out.println("El total es: "+total4);
@@ -127,6 +136,8 @@ public class ProyectoFinalProgramacion
                 int subtotal5= (Cabezote2);
                 double ap_iva5= (subtotal5*iva);
                 double total5= (subtotal5+ap_iva5);
+                
+                System.out.println("***Taller Goldoni***");
                 System.out.println("Valor de IVA: "+iva);
                 System.out.println("El subtotal es: "+subtotal5);
                 System.out.println("El total es: "+total5);
@@ -136,15 +147,83 @@ public class ProyectoFinalProgramacion
                 int subtotal6= (Cabezote3);
                 double ap_iva6= (subtotal6*iva);
                 double total6= (subtotal6+ap_iva6);
+                
+                System.out.println("***Taller Goldoni***");
                 System.out.println("Valor de IVA: "+iva);
                 System.out.println("El subtotal es: "+subtotal6);
                 System.out.println("El total es: "+total6);
+            }
+            if (opcion==7)
+            {
+                int subtotal7= (Longitud1);
+                double ap_iva7= (subtotal7*iva);
+                double total7= (subtotal7+ap_iva7);
+                
+                System.out.println("***Taller Goldoni***");
+                System.out.println("Valor de IVA: "+iva);
+                System.out.println("El subtotal es: "+subtotal7);
+                System.out.println("El total es: "+total7);   
+            }
+            if (opcion==8)
+            {
+                int subtotal8= (Longitud2);
+                double ap_iva8= (subtotal8*iva);
+                double total8= (subtotal8+ap_iva8);
+                
+                System.out.println("***Taller Goldoni***");
+                System.out.println("Valor de IVA: "+iva);
+                System.out.println("El subtotal es: "+subtotal8);
+                System.out.println("El total es: "+total8);  
+            }
+            if (opcion==9)
+            {
+                int subtotal9= (Longitud3);
+                double ap_iva9= (subtotal9*iva);
+                double total9= (subtotal9+ap_iva9);
+                
+                System.out.println("***Taller Goldoni***");
+                System.out.println("Valor de IVA: "+iva);
+                System.out.println("El subtotal es: "+subtotal9);
+                System.out.println("El total es: "+total9);   
+            }
+            if (opcion==10)
+            {
+                int subtotal10= (Torque1);
+                double ap_iva10= (subtotal10*iva);
+                double total10= (subtotal10+ap_iva10);
+                
+                System.out.println("***Taller Goldoni***");
+                System.out.println("Valor de IVA: "+iva);
+                System.out.println("El subtotal es: "+subtotal10);
+                System.out.println("El total es: "+total10);   
+            }
+            if (opcion==11)
+            {
+                int subtotal11= (Torque2);
+                double ap_iva11= (subtotal11*iva);
+                double total11= (subtotal11+ap_iva11);
+                
+                System.out.println("***Taller Goldoni***");
+                System.out.println("Valor de IVA: "+iva);
+                System.out.println("El subtotal es: "+subtotal11);
+                System.out.println("El total es: "+total11);   
+            }
+            if (opcion==12)
+            {
+                int subtotal12= (Torque1);
+                double ap_iva12= (subtotal12*iva);
+                double total12= (subtotal12+ap_iva12);
+                
+                System.out.println("***Taller Goldoni***");
+                System.out.println("Valor de IVA: "+iva);
+                System.out.println("El subtotal es: "+subtotal12);
+                System.out.println("El total es: "+total12);   
             }
         }
         
         public void Salir()
             {
-               JOptionPane.showMessageDialog(null,"Usted ha salido del sistema"); 
+               JOptionPane.showMessageDialog(null,"Gracias por su compra en el Taller Goldoni"); 
             }
         public static void main(String[] args)
         {
@@ -152,5 +231,6 @@ public class ProyectoFinalProgramacion
             FIDE1.Datos_Personales();
             FIDE1.Precios();
             FIDE1.Factura();
+            FIDE1.Salir();
         }
 }
